21b10febc6714dd316fa666047453480bf2c8f7f
Matías <matias@shishi>
2016-10-12 21:34:35 -0300

    Close #2 - termino k-select, fixes menores.
    * Se solucionó el bug al ejecutar el algoritmo k-select con k>1.
    * Se cambiaron los nombres de las variables en los tests a nombres más descriptivos.

--- tp1/src/statistic/k_select.rs
+++ tp1/src/statistic/k_select.rs
@@ -4,16 +4,13 @@ pub fn k_statistic(numbers: &Vec<i32>, k: i32) -> i32 {
 	let mut min_idx = 0;
 	for current_k in 0 .. (k+1) as usize {
 		for idx in current_k .. numbers.len() {
-			if numbers[idx] < numbers[min_idx] {
-				min_idx = idx
+			if nums_cpy[idx] < nums_cpy[min_idx] {
+				min_idx = idx;
 			}
 		}
 		let aux = nums_cpy[min_idx];
 		nums_cpy[min_idx] = nums_cpy[current_k];
 		nums_cpy[current_k] = aux;
-	}
-	for (idx, item) in nums_cpy.iter().enumerate() {
-		println!("Vector[{}]: {}", idx, item);
 	}
 	nums_cpy[k as usize]
 }
